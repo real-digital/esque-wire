38ec86efaa28b9e221d970f38e983931aceb8328
Swen Wenzel <swen.wenzel@real-digital.de>
2019-11-22 10:02:39 +0100

    fix test invocation

--- tests/integration/test_protocol_messages.py
+++ tests/integration/test_protocol_messages.py
@@ -1,9 +1,12 @@
 from typing import List
 
+import pytest
+
 from esque_wire.connection import BrokerConnection
 from esque_wire.protocol.structs.api import ApiVersionsRequestData, MetadataRequestData
 
 
+@pytest.mark.integration
 def test_api_versions(connection: BrokerConnection):
     data = ApiVersionsRequestData()
 
@@ -11,6 +14,7 @@ def test_api_versions(connection: BrokerConnection):
     assert len(response.response_data.api_versions) > 0
 
 
+@pytest.mark.integration
 def test_metadata(connection: BrokerConnection, bootstrap_servers: List[str]):
     data = MetadataRequestData([], False, True, True)
 
