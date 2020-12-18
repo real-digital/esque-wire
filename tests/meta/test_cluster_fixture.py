5a25e0a6c98faa40995b2f0004d953827ce22379
Swen <swen.wenzel@real-digital.de>
2020-12-19 00:52:46 +0100

    add smoke test for cluster fixture

---
+++ tests/meta/test_cluster_fixture.py
@@ -0,0 +1,6 @@
+from cluster_fixture import Cluster
+
+
+def test_simple_startup(kafka_version):
+    with Cluster(kafka_version=kafka_version) as cluster:
+        cluster.boostrap_servers("PLAINTEXT")
