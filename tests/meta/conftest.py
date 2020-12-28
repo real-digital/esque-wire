5f0f1ded58b12952da7b02a832606b17a943d158
Swen <swen.wenzel@real-digital.de>
2020-12-28 12:32:12 +0100

    use multiple kafka versions for fixture smoke test

---
+++ tests/meta/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from _pytest.fixtures import SubRequest
+from cluster_fixture import KafkaVersion
+
+TESTED_KAFKA_VERSIONS = ["1.1.1", "2.0.0", "2.1.0", "2.2.0", "2.3.0", "2.4.0"]
+
+
+@pytest.fixture(params=TESTED_KAFKA_VERSIONS, ids=[f"kafka-{v.replace('.', '_')}" for v in TESTED_KAFKA_VERSIONS])
+def kafka_version(request: SubRequest) -> KafkaVersion:
+    return KafkaVersion(request.param)
