366b4f537cc6de8da240ff6d4f1b93ffd310ec6f
Swen <swen.wenzel@real-digital.de>
2020-12-28 13:45:05 +0100

    fix missing import in cluster_fixture

--- tests/cluster_fixture/__init__.py
+++ tests/cluster_fixture/__init__.py
@@ -1,9 +1,17 @@
 from tests.cluster_fixture.cluster import Cluster
-from tests.cluster_fixture.kafka import KafkaInstance, KafkaVersion, PlaintextEndpoint, SaslEndpoint, SaslMechanism
+from tests.cluster_fixture.kafka import (
+    DEFAULT_KAFKA_VERSION,
+    KafkaInstance,
+    KafkaVersion,
+    PlaintextEndpoint,
+    SaslEndpoint,
+    SaslMechanism,
+)
 from tests.cluster_fixture.zookeeper import ZookeeperInstance
 
 __all__ = [
     "Cluster",
+    "DEFAULT_KAFKA_VERSION",
     "KafkaInstance",
     "KafkaVersion",
     "PlaintextEndpoint",
