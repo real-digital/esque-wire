97752231f3c11c1d1b7e14137ba7cccba2ecbc0d
Swen <swen.wenzel@real-digital.de>
2021-03-10 15:29:07 +0100

    provide Endpoint in cluster_fixture.__init__

--- tests/cluster_fixture/__init__.py
+++ tests/cluster_fixture/__init__.py
@@ -7,6 +7,7 @@ Check :class:`Cluster` for more information about how to use and configure it.
 from tests.cluster_fixture.cluster import Cluster
 from tests.cluster_fixture.kafka import (
     DEFAULT_KAFKA_VERSION,
+    Endpoint,
     KafkaInstance,
     KafkaVersion,
     PlaintextEndpoint,
@@ -20,6 +21,7 @@ from tests.cluster_fixture.zookeeper import ZookeeperInstance
 __all__ = [
     "Cluster",
     "DEFAULT_KAFKA_VERSION",
+    "Endpoint",
     "KafkaInstance",
     "KafkaVersion",
     "PlaintextEndpoint",
