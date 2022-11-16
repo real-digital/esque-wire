3586bbd514d63b0286ba8e823a5d1c54ad0d6460
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 00:40:50 +0000

    Adding tarfile member sanitization to extractall()

--- cluster_fixture/base.py
+++ cluster_fixture/base.py
@@ -507,7 +507,26 @@ def download_kafka(kafka_version: KafkaVersion, destination: Path) -> None:
         logger.info(f"Extracting kafka archive to {destination}")
         with tarfile.open(tmpfile.name) as tar:
             members = [mem for mem in tar.getmembers() if should_extract(mem)]
-            tar.extractall(members=members, path=str(destination))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, members=members, path=str(destination))
 
 
 async def netcat_async_string(loop: asyncio.AbstractEventLoop, hostname: str, port: int, content: str) -> str:
